250de0614aaf5e4416839c0eac906e4c9e043bc2
derisan <derisan@naver.com>
2021-11-23 09:04:14 +0900

    Add Systems::Intersects

--- NGP_Pong/Systems.cpp
+++ NGP_Pong/Systems.cpp
@@ -19,3 +19,38 @@ void Systems::UpdatePosition(float speed, const Vector2& direction, Vector2& out
 	outPosition.x += speed * direction.x * deltaTime;
 	outPosition.y += speed * direction.y * deltaTime;
 }
+
+bool Systems::Intersects(const SDL_Rect& a, const SDL_Rect& b)
+{
+	int leftA = a.x;
+	int rightA = a.x + a.w;
+	int topA = a.y;
+	int bottomA = a.y + a.h;
+
+	int leftB = b.x;
+	int rightB = b.x + b.w;
+	int topB = b.y;
+	int bottomB = b.y + b.h;
+
+	if (bottomA <= topB)
+	{
+		return false;
+	}
+
+	if (topA >= bottomB)
+	{
+		return false;
+	}
+
+	if (rightA <= leftB)
+	{
+		return false;
+	}
+
+	if (leftA >= rightB)
+	{
+		return false;
+	}
+
+	return true;
+}
--- NGP_Pong/Systems.h
+++ NGP_Pong/Systems.h
@@ -5,4 +5,5 @@ class Systems
 public:
 	static void DrawRect(SDL_Renderer* renderer, float w, float h, const Vector2& position);
 	static void UpdatePosition(float speed, const Vector2& direction, Vector2& outPosition, float deltaTime = 0.0166f);
+	static bool Intersects(const SDL_Rect& a, const SDL_Rect& b);
 };
