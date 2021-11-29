95623c90e9dc720fba3af64b208d5e7832081428
Park Jin Woo <pjw1480@naver.com>
2021-11-29 22:08:01 +0900

    Mypaddle Color Change Red

--- NGP_Pong/Systems.h
+++ NGP_Pong/Systems.h
@@ -10,6 +10,7 @@ enum class CollisionSide
 class Systems
 {
 public:
+	static void DrawRectColor(SDL_Renderer* renderer, float w, float h, const Vector2& position);
 	static void DrawRect(SDL_Renderer* renderer, float w, float h, const Vector2& position);
 	static void UpdatePosition(float speed, const Vector2& direction, Vector2& outPosition, float deltaTime = 0.0166f);
 	static void AdjustXPos(const Vector2& paddlePosition, const Vector2& ballDirection, Vector2& outBallPosition);
